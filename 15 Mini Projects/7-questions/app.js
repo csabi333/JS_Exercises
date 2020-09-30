35935dbe57c29002f88443e86dddead389247754
Csaba Puskás <36250394+csabi333@users.noreply.github.com>
2020-09-30 15:33:09 +0200

    using selectors inside the element

--- 15 Mini Projects/7-questions/app.js
+++ 15 Mini Projects/7-questions/app.js
@@ -1,10 +1,26 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
+/* const btns = document.querySelectorAll('.question-btn');
 
 btns.forEach(function(btn){
 btn.addEventListener('click',function(e){
     e.currentTarget.parentElement.parentElement.classList.toggle('show-text')
 })
+}) */
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    //console.log(question);
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click',function(){
+        questions.forEach(function(item){
+            if(item !=question){
+            item.classList.remove('show-text');
+        }})
+        question.classList.toggle('show-text');
+    })
 })
+
+
